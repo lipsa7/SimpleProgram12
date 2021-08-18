22880b8d5667bc5b4a4fed27fa755feb72f3341c
lipsa7 <lipsakar98@gmail.com>
2021-08-18 20:24:59 +0530

    Add project files.

---
+++ Rdppractice10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdppractice10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            Console.WriteLine(UserProgramCode.testValid(input1, input2));
+            Console.ReadKey();
+
+        }
+    }
+
+    class UserProgramCode
+    {
+        public static string testValid(string input1, string input2)
+        {
+            if (input1.Substring(0, 4) == "CTS-")
+                if (input1.Substring(4, 3) == input2.Substring(0, 3))
+                    if (input1.Substring(7, 1) == "-")
+                        if (int.Parse(input1.Substring(8, 4)) <= 9999 && int.Parse(input1.Substring(8, 4)) >= 0)
+                            return "Valid";
+                        else
+                            return "Not Valid";
+
+
+
+            return "a";
+        }
+    }
+}
